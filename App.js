74d59513437343e0b8d3cd2d1bf0f9c153584bd5
ELSS-coder <emerson2016.1@gmail.com>
2025-04-22 20:18:56 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div style={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+      fontFamily: 'sans-serif'
+    }}>
+      <h1>UniFlow</h1>
+      <p>Seu organizador universitário PWA está pronto!</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ index.js
@@ -0,0 +1,1 @@
+import React from 'react'; import ReactDOM from 'react-dom/client'; import Aplicativo from './Aplicativo'; const root = ReactDOM.createRoot(document.getElementById('root')); root.render(<Aplicativo />);
